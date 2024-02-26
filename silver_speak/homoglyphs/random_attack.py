e68e8e40c415e525f8749c05ecb47ed475c4e6ed
ACMCMC <>
2024-02-26 23:29:43 +0000

    Random attack - change percentage

--- silver_speak/homoglyphs/random_attack.py
+++ silver_speak/homoglyphs/random_attack.py
@@ -3,18 +3,19 @@ from silver_speak.homoglyphs.identical_map import chars_map
 import random
 
 
-def random_attack(text: str, probability=0.5, random_seed=42) -> str:
+def random_attack(text: str, percentage=0.1, random_seed=42) -> str:
     """
     Replaces some characters in the text, randomly choosing which ones, leaving all others unchanged.
     """
     random.seed(random_seed)
-    # Replace some characters in the text
-    return "".join(
-        (
-            char
-            if random.random() > probability
-            else chars_map.get(char, [char])
-        )
-        for char in text
-    )
+    # Replace some characters in the text with their equivalent characters from the chars_map
+    num_to_replace = int(len(text) * percentage)
+    text = list(text)
+    while num_to_replace > 0:
+        position = random.randint(0, len(text) - 1)
+        char = text[position]
+        if char in chars_map:
+            text[position] = chars_map[char]
+            num_to_replace -= 1
+    return "".join(text)
 # %%
