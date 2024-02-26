0d80b7584a9f8cc8ffb89b9b01d435c3bdbd3bce
ACMCMC <>
2024-02-26 22:41:15 +0000

    Delete old files

--- evaluate.sh
+++
@@ -1,9 +0,0 @@
-export DATASETS_VERBOSITY=info
-export EVALUATE_VERBOSITY=info
-export TRANSFORMERS_VERBOSITY=info
-
-workon silverspeak
-
-echo "Evaluating with $num_examples examples and $attack attack"
-
-python -u evaluate.py --num_examples $num_examples --attack $attack --detection_system $detection_system
