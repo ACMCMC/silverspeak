1720780858d3ed20899d1280803716cca4294c0f
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2025-01-26 16:15:26 +0100

    v2.3.0

--- silverspeak/homoglyphs/utils.py
+++ silverspeak/homoglyphs/utils.py
@@ -1,8 +1,10 @@
 import json
 import random
 import unicodedata
-from typing import List, Literal, Mapping, Set
 from pathlib import Path
+from typing import List, Literal, Mapping, Set, Tuple
+
+from uniscripts import get_script_counts, is_script
 
 
 class HomoglyphReplacer:
@@ -42,13 +44,14 @@ class HomoglyphReplacer:
         self.unicode_categories_to_replace = unicode_categories_to_replace
         self.types_of_homoglyphs_to_use = types_of_homoglyphs_to_use
         self.replace_with_priority = replace_with_priority
-        self.chars_map: Mapping[str, List[str]] = self._load_chars_map()
+        self.chars_map = self._load_chars_map()
         # This object will be used to keep the random state
         self.random_state = random.Random(x=random_seed)
         self.reverse_chars_map: Mapping[str, str] = self._create_reverse_chars_map()
-        self.normalization_table = self._create_normalization_table()
+        self._base_normalization_map = self._create_base_normalization_map()
+        self.normalization_translation_tables = {}
 
-    def _load_chars_map(self):
+    def _load_chars_map(self) -> Mapping[str, List[str]]:
         """
         Load the character mappings from JSON files.
 
@@ -89,24 +92,28 @@ class HomoglyphReplacer:
         reverse_map = {}
         for key, values in self.chars_map.items():
             for value in values:
-                reverse_map[value] = key
+                # If there's already a key for this value, don't overwrite it - the first one we find is the highest priority
+                if value not in reverse_map:
+                    reverse_map[value] = key
+
         return reverse_map
 
-    def _create_normalization_table(self):
+    def _create_base_normalization_map(self):
         """
-        Create a table for normalizing text.
+        Create a base normalization map for the most common homoglyphs.
 
         Returns:
-            dict: Table for normalizing text.
+            dict: Base normalization map.
         """
         # Don't include any entries for characters that are in NFKD normalization form
         # I.e., only include entries for characters that are not in normalization form (which are the homoglyphs)
-        nfc_entries = {
+        # Also, only include entries for characters that are in list of unicode categories to replace
+        base_normalization_map = {
             key: value
             for key, value in self.reverse_chars_map.items()
-            if not unicodedata.is_normalized("NFKD", key)
+            if not unicodedata.is_normalized("NFKD", key) and unicodedata.category(key) in self.unicode_categories_to_replace
         }
-        return str.maketrans(nfc_entries)
+        return base_normalization_map
 
     def is_replaceable(self, char: str) -> bool:
         """
@@ -159,9 +166,54 @@ class HomoglyphReplacer:
         """
         return self.reverse_chars_map[char]
 
+    def _detect_dominant_script(self, text: str) -> str:
+        """
+        Detect the dominant script in the text.
+
+        Args:
+            text (str): Text to analyze.
+
+        Returns:
+            str: Dominant script in the text.
+        """
+        script_counts = get_script_counts(text)
+        return max(script_counts, key=script_counts.get)
+
+    def _get_normalization_table_for_script(self, script: str) -> dict:
+        """
+        Get the normalization table for a script.
+
+        Args:
+            script (str): Script to get the normalization table for.
+
+        Returns:
+            dict: Normalization table for the script.
+        """
+        # Check if it's in our in-memory cache
+        if script in self.normalization_translation_tables:
+            return self.normalization_translation_tables[script]
+
+        # Generate the normalization table.
+        base_normalization_map = self._base_normalization_map
+
+        # Create a dictionary with the NFC entries where the value is in the script we want to normalize (i.e. (this_or_other_script, script) pairs)
+        script_normalization_map = {
+            key: value
+            for key, value in base_normalization_map.items()
+            if is_script(value, script)
+        }
+        script_normalization_translation_table = str.maketrans(script_normalization_map)
+
+        self.normalization_translation_tables[script] = (
+            script_normalization_translation_table
+        )
+
+        return script_normalization_translation_table
+
     def normalize(self, text: str) -> str:
         """
-        Normalize text by replacing homoglyphs with their original characters.
+        Normalize text by replacing homoglyphs with their original characters,
+        based on the dominant script in the text.
 
         Args:
             text (str): Text to normalize.
@@ -169,4 +221,6 @@ class HomoglyphReplacer:
         Returns:
             str: Normalized text.
         """
-        return text.translate(self.normalization_table)
+        dominant_script = self._detect_dominant_script(text)
+        normalization_table = self._get_normalization_table_for_script(dominant_script)
+        return text.translate(normalization_table)
